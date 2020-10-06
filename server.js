e8bef4eebbd7b41154b98a6768fdbd4a156c1a5b
Rikke-Jorgensen <rikke4800@hotmail.com>
2020-10-06 21:30:22 +1300

    path turned to string

--- server.js
+++ server.js
@@ -13,38 +13,38 @@ server.use(express.static(path.join(__dirname, 'public')))
 
 server.get('/', function(req, res) {
     var hostname = req.headers.host;
-    if (hostname === rikkesimone.com) {
+    if (hostname === 'rikkesimone.com') {
         res.sendFile(path.join(__dirname, 'public', 'portfolioindex.html'))
     }   
-    else if (hostname === pawsomepackadventures.co.nz) {
+    else if (hostname === 'pawsomepackadventures.co.nz') {
         res.sendFile(path.join(__dirname, 'public', 'lisaindex.html'))
     }   
 })
 
 server.get('/home', function(req, res) {
     var hostname = req.headers.host;   
-    if (hostname === pawsomepackadventures.co.nz) {
+    if (hostname === 'pawsomepackadventures.co.nz') {
         res.sendFile(path.join(__dirname+'/lisa/client/components/Home.jsx'))
     }   
 })
 
 server.get('/info', function(req, res) {
     var hostname = req.headers.host;   
-    if (hostname === pawsomepackadventures.co.nz) {
+    if (hostname === 'pawsomepackadventures.co.nz') {
         res.sendFile(path.join(__dirname+'/lisa/client/components/Info.jsx'))
     }   
 })
 
 server.get('/gallery', function(req, res) {
     var hostname = req.headers.host;   
-    if (hostname === pawsomepackadventures.co.nz) {
+    if (hostname === 'pawsomepackadventures.co.nz') {
         res.sendFile(path.join(__dirname+'/lisa/client/components/Gallery.jsx'))
     }   
 })
 
 server.get('/contact', function(req, res) {
     var hostname = req.headers.host;   
-    if (hostname === pawsomepackadventures.co.nz) {
+    if (hostname === 'pawsomepackadventures.co.nz') {
         res.sendFile(path.join(__dirname+'/lisa/components/Form.jsx'))
     }   
 })
